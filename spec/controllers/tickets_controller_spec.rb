cd1656960b0801146c59279fc597f9dcd9dae974
Americo Savinon <americos@gmail.com>
2010-10-15 15:22:16 -0400

    Restrict reading tickets to correct project scope

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,4 +2,22 @@ require 'spec_helper'
 
 describe TicketsController do
 
+  before do
+    stub_warden
+  end
+  
+  let(:user) { User.create(:email => "user@ticketee.com", :password => "password", :password_confirmation => "password")}
+  let(:project) { Project.create(:name => "Ticketee")}
+  let(:ticket) { project.tickets.create(:title => "Restrict Permissions", :description => "For the tickets controller.")}
+
+  context "standard users" do
+    it "cannot access a ticket for a project they don't have access to" do
+      sign_in_as(user)
+      get :show, :id => ticket.id, :project_id => project.id
+      response.should redirect_to(root_path)
+      flash[:alert].should eql("The project you were looking for could not be found.")
+    end
+  end
+  
+
 end
